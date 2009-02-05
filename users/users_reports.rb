983c9874ff5c4755045c63520e4408a71557b640
Pallay Raunu <pallay@ensembli.com>
2009-02-05 14:45:18 +0000

    added active user and user churn

--- users/users_reports.rb
+++ users/users_reports.rb
@@ -9,8 +9,9 @@ class UsersReports < Scout::Plugin
     ignore_users = "id not in(1,2,3,11,12,13,14,15,17,21,24,29)"
     begin
       require "#{option('path_to_app')}/config/environment.rb"
-      report(:total_users => User.count(:conditions => ignore_users),
-             :active_users => User.activity_within(1.week).size
+      report(:total_users  => User.count(:conditions => ignore_users),
+             :active_users => User.find(:all, :conditions => ['user_logs.created_at > ?', Time.now - 1.week], :select => 'users.id, users.login', :joins => 'LEFT JOIN user_logs ON user_logs.user_id = users.id', :group => 'users.id').size
+             :user_churn  => User.find(:all, :conditions => ['user_logs.created_at < ?', Time.now - 6.months], :select => 'users.id, users.login', :joins => 'LEFT JOIN user_logs ON user_logs.user_id = users.id', :group => 'users.id').size
       )
     rescue
       error(:subject => "Unable to monitor Users",
